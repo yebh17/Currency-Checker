0645b0dc708b5f9634ea04bd91847a798273625f
yebh17 <sunnyrebels0@gmail.com>
2021-02-08 17:23:33 +0100

    Add dataframe and store data to data.csv file

--- scrape.py
+++ scrape.py
@@ -1,32 +1,27 @@
-
-# import module 
-import requests  
-import bs4 
+from contextlib import redirect_stdout
 from datetime import datetime
-import sched, time
-s = sched.scheduler(time.time, time.sleep)
+import bs4 
+import requests  
+import pandas as pd
 
-def scheduler(sc): 
+now = datetime.now()
 
-    now = datetime.now()
+while True:
 
     current_time = now.strftime("%H:%M:%S")
-  
-    # Generating the url   
-    url = "https://www.google.com/search?q=sek+to+inr&rlz=1C5CHFA_enSE926SE927&oq=sek+to+i&aqs=chrome.1.69i57j35i39j0l4j0i457j69i60.4923j1j9&sourceid=chrome&ie=UTF-8"
-  
-    # Sending HTTP request  
+
+    url = "https://www.google.com/search?q=sek+to+inr&rlz=1C5CHFA_enSE926SE927&oq=sek+to+i&aqs=chrome.1.69i57j35i39j0l4j0i457j69i60.4923j1j9&sourceid=chrome&ie=UTF-8"  
+
     request_result = requests.get( url ) 
-  
-    # Pulling HTTP data from internet  
+
     soup = bs4.BeautifulSoup( request_result.text  
                             , "html.parser" ) 
-  
-    temp = soup.find( "div" , class_='BNeawe' ).text  
-    
-    print ( current_time, ':', temp )
+        
+    temp = soup.find( "div" , class_='BNeawe' ).text 
+            
+    data = {'Time': [current_time], 
+            'Currency': [temp]
+                    } 
 
-    s.enter(30, 1, scheduler, (sc,))
-
-s.enter(30, 1, scheduler, (s,))
-s.run()
+    df = pd.DataFrame(data, columns = ['Time', 'Currency'])
+    df.to_csv (r'data.csv', sep = '|', mode='a', index = False, header=True)
