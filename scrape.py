960208383513a487893485725926180621a774d4
Ubuntu <ubuntu@ip-172-31-30-59.eu-north-1.compute.internal>
2021-02-14 22:06:33 +0000

    Major changes

--- scrape.py
+++ scrape.py
@@ -1,27 +1,28 @@
+#!/usr/bin/python3
+
 from contextlib import redirect_stdout
 from datetime import datetime
 import bs4 
 import requests  
 import pandas as pd
 
-now = datetime.now()
+url = "https://www.google.com/search?q=sek+to+inr&rlz=1C5CHFA_enSE926SE927&oq=sek+to+i&aqs=chrome.1.69i57j35i39j0l4j0i457j69i60.4923j1j9&sourceid=chrome&ie=UTF-8"  
 
-while True:
+request_result = requests.get( url ) 
 
-    current_time = now.strftime("%H:%M:%S")
-
-    url = "https://www.google.com/search?q=sek+to+inr&rlz=1C5CHFA_enSE926SE927&oq=sek+to+i&aqs=chrome.1.69i57j35i39j0l4j0i457j69i60.4923j1j9&sourceid=chrome&ie=UTF-8"  
-
-    request_result = requests.get( url ) 
-
-    soup = bs4.BeautifulSoup( request_result.text  
-                            , "html.parser" ) 
+soup = bs4.BeautifulSoup( request_result.text  
+                    , "html.parser" ) 
         
-    temp = soup.find( "div" , class_='BNeawe' ).text 
-            
-    data = {'Time': [current_time], 
-            'Currency': [temp]
-                    } 
+temp = soup.find( "div" , class_='BNeawe' ).text
 
-    df = pd.DataFrame(data, columns = ['Time', 'Currency'])
-    df.to_csv (r'data.csv', sep = '|', mode='a', index = False, header=True)
+value = temp[:4]
+
+value = value.replace(',','.')
+            
+data = {'Currency': [value]} 
+
+df = pd.DataFrame(data)
+
+output = df.to_csv (mode = 'a', index = False, header=True)
+
+print (output)
