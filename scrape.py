b40e03d31aa38f0e42b4bf632be5dbda8539ebb0
yebh17 <sunnyrebels0@gmail.com>
2021-05-13 16:05:31 +0200

    Add data frames to the values output

--- scrape.py
+++ scrape.py
@@ -1,9 +1,19 @@
 #!/usr/local/bin/python3
 
 from forex_python.converter import CurrencyCodes, CurrencyRates
+import pandas as pd
 
 test = CurrencyRates()
 
 #fetch the Conversion rate
 rate = test.get_rate('SEK', 'INR')
-print(rate)
+
+data = {'Currency': [rate]} 
+
+df = pd.DataFrame(data)
+
+output = df.to_csv (mode = 'a', index = False, header=True)
+
+df = pd.DataFrame(data, columns = ['Time', 'Currency'])
+df.to_csv (r'data.csv', sep = '|', mode='a', index = False, header=True)
+print (output)
