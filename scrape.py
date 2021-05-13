18edee7a140aa5c08849a76222b757269f89738c
yebh17 <sunnyrebels0@gmail.com>
2021-05-13 15:56:27 +0200

    Using a forex_python library

--- scrape.py
+++ scrape.py
@@ -1,28 +1,9 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
-from contextlib import redirect_stdout
-from datetime import datetime
-import bs4 
-import requests  
-import pandas as pd
+from forex_python.converter import CurrencyCodes, CurrencyRates
 
-url = "https://www.google.com/search?q=sek+to+inr&rlz=1C5CHFA_enSE926SE927&oq=sek+to+i&aqs=chrome.1.69i57j35i39j0l4j0i457j69i60.4923j1j9&sourceid=chrome&ie=UTF-8"  
+test = CurrencyRates()
 
-request_result = requests.get( url ) 
-
-soup = bs4.BeautifulSoup( request_result.text  
-                    , "html.parser" ) 
-        
-temp = soup.find( "div" , class_='BNeawe' ).text
-
-value = temp[:4]
-
-value = value.replace(',','.')
-            
-data = {'Currency': [value]} 
-
-df = pd.DataFrame(data)
-
-output = df.to_csv (mode = 'a', index = False, header=True)
-
-print (output)
+#fetch the Conversion rate
+rate = test.get_rate('SEK', 'INR')
+print(rate)
